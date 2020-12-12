bff648b2bbe6145b0c580c9f79449aa8ac5db111
Chris <silverchris@gmail.com>
2020-12-12 17:10:12 -0500

    fix focus with backup cam?

--- src/autoapp/Managers/VideoManager.cpp
+++ src/autoapp/Managers/VideoManager.cpp
@@ -21,18 +21,18 @@ VideoManager::VideoManager(DBus::Connection &hmiBus, VideoSignals::Pointer video
 
 void VideoManager::requestFocus(VIDEO_FOCUS_REQUESTOR requestor)
 {
-//    if (bucpsa.currentDisplayMode && requestor == VIDEO_FOCUS_REQUESTOR::ANDROID_AUTO) {
-//        // we can safely exit - backup camera will notice us when finish and we request focus back
-//        waitsForFocus = true;
-//        return;
-//    }
-//    if (waitsForFocus && requestor == VIDEO_FOCUS_REQUESTOR::BACKUP_CAMERA)
-//    {
-//        // need to wait for a second (maybe less but 100ms is too early) to make sure
-//        // the CMU has already changed the surface from backup camera to opera
-//        sleep(1);
-//        waitsForFocus = false;
-//    }
+    if (bucpsa.currentDisplayMode && requestor == VIDEO_FOCUS_REQUESTOR::ANDROID_AUTO) {
+        // we can safely exit - backup camera will notice us when finish and we request focus back
+        waitsForFocus = true;
+        return;
+    }
+    if (waitsForFocus && requestor == VIDEO_FOCUS_REQUESTOR::BACKUP_CAMERA)
+    {
+        // need to wait for a second (maybe less but 100ms is too early) to make sure
+        // the CMU has already changed the surface from backup camera to opera
+        sleep(1);
+        waitsForFocus = false;
+    }
     LOG(DEBUG) << "Setting focus, requested by " << static_cast<std::underlying_type<VIDEO_FOCUS_REQUESTOR>::type>(requestor);
     gui.SetRequiredSurfacesByEnum(NativeGUICtrlClient::TV_TOUCH_SURFACE, true);
 }
@@ -40,7 +40,7 @@ void VideoManager::requestFocus(VIDEO_FOCUS_REQUESTOR requestor)
 void VideoManager::releaseFocus(VIDEO_FOCUS_REQUESTOR requestor)
 {
     LOG(DEBUG) << "Releasing focus, requested by " << static_cast<std::underlying_type<VIDEO_FOCUS_REQUESTOR>::type>(requestor);
-    gui.SetRequiredSurfacesByEnum({NativeGUICtrlClient::JCI_OPERA_PRIMARY}, true);
+    gui.SetRequiredSurfacesByEnum(NativeGUICtrlClient::JCI_OPERA_PRIMARY, true);
 }
 
 VideoManager::~VideoManager()= default;
